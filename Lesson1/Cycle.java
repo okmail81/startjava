00b650eb7f14a763bbacd60038cf21e08c330875
okmail81 <okmail81@mail.ru>
2022-02-19 19:03:24 +0300

    hv1 Cycle

---
+++ Lesson1/Cycle.java
@@ -0,0 +1,30 @@
+public class Cycle {
+    public static void main(String[] args) {
+        //1. Выведите на консоль с помощью цикла for все числа от [0, 20]
+        System.out.println("\n1. Выведите на консоль с помощью цикла for все числа от [0, 20]");
+        for (int i = 0; i <= 20; i++) {
+            System.out.println("Число " + i);
+        }
+        
+        //2. Выведите на консоль с помощью цикла while все числа от [6, -6] (с шагом итерации 2)
+        System.out.println("\n2. Выведите на консоль с помощью цикла while все числа от [6, -6] (с шагом итерации 2)");
+        
+        int i = 6;
+        while (i >= -6) {
+            System.out.println("Число " + i);
+            i = i - 2;
+        }
+        //3. Подсчитайте с помощью цикла do-while сумму нечетных чисел от [10, 20]. Выведите ее на консоль
+        System.out.println("\n3. Подсчитайте с помощью цикла do-while сумму нечетных чисел от [10, 20]. Выведите ее на консоль");
+        i = 10;
+        int numSum = 0;
+        do {
+            if (i % 2 > 0) {
+                System.out.println("Число " + i);
+                numSum = numSum + i;
+            }
+            i++;
+        } while(i <= 20);
+        System.out.println("Cумма нечетных чисел от [10, 20] - " + numSum);
+    }
+}
